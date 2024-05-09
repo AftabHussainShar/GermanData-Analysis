c89a116b737df891df573b718566047fecf3323a
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-05-09 13:48:40 +0500

    Code + Docx + PDF

---
+++ LoadLibrary.R
@@ -0,0 +1,19 @@
+load.library <- function()
+{
+      library(lattice)
+      library(ggplot2)
+      library(caret)
+      library(gplots)
+      library(ROCR)
+      library(dplyr)
+      library(ROSE)
+  
+      library(rpart)
+      library("grid")
+      library("MASS")
+      library("neuralnet")
+      library(kernlab)
+      library(randomForest)
+      library(class)
+      library(sperrorest)
+}
